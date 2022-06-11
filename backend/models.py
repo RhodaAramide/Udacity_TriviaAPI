65b480e8e7e00c778849ac32f50e0492ad3bc0a2
RhodaAramide <olabisiabolarin5@gmail.com>
2022-06-11 16:48:14 +0100

    Setting environment variables

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,8 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-
-database_name = 'trivia'
-database_path = "postgresql://{}:{}@{}/{}".format(
-    "postgres", "abolarin", "localhost:5432", database_name
-)
+from dotenv import load_dotenv
+load_dotenv()
 
 db = SQLAlchemy()
 
@@ -14,8 +11,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,24 +5,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
+        """Define test variables and initialize app."""        
         self.app = create_app()
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_PATH")
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "postgres", "abolarin", "localhost:5432", self.database_name
-        )
-        setup_db(self.app, self.database_path)
+        # self.database_path = os.environ.get("DATABASE_PATH")
+        
+        # setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
+            
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
@@ -97,14 +99,14 @@ class TriviaTestCase(unittest.TestCase):
 
     # Delete a different question in each attempt
     def test_delete_question(self):
-        res = self.client().delete("/questions/17")
+        res = self.client().delete("/questions/13")
         data = json.loads(res.data)
 
-        question = Question.query.filter(Question.id == 17).one_or_none()
+        question = Question.query.filter(Question.id == 13).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
-        self.assertEqual(data["deleted"], 17)
+        self.assertEqual(data["deleted"], 13)
         self.assertEqual(question, None)
 
     def test_422_if_question_does_not_exist(self):
